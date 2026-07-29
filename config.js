89fdd1686ae88a2eff54a5a3b8d0a928bac96214
hg679830-glitch <hg679830@gmail.com>
2026-07-29 10:51:45 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Pega aquí la URL de tu Google Apps Script publicado como aplicación web.
 // Instrucciones completas en README.md → "Conectar con Google Sheets".
-const SCRIPT_URL = "PEGA_AQUI_TU_URL_DE_APPS_SCRIPT";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzEkmEagpazECZyVCmDUZJyZ100ALkPRB0_9Dwa0jYHPQSDlTULf0kcuKq_zWdRPVLd1g/exec";
